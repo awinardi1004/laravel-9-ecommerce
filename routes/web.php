1491eb5875f25ef14183736d99dfcd02ba983d5c
awinardi1004 <awinardi1004@gmail.com>
2025-03-15 11:38:04 +0700

    crate and show product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/product/create', [ProductController::class, 'create_product'])->name('create_product');
+Route::post('/product/create', [ProductController::class, 'store_product'])->name('store_product');
+Route::get('/product', [ProductController::class, 'index_product'])->name('index_product');
